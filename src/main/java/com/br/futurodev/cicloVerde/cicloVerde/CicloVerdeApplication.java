54407f923efa4eba7d1f6c87b91699370e08995e
wellingtondebrito <wellingtondebritosouza@gmail.com>
2025-04-07 10:38:45 -0300

    feat: inciar o projeto Ciclo Verde com estrutura base Spring boot

---
+++ src/main/java/com/br/futurodev/cicloVerde/cicloVerde/CicloVerdeApplication.java
@@ -0,0 +1,13 @@
+package com.br.futurodev.cicloVerde.cicloVerde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicloVerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicloVerdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/futurodev/cicloVerde/cicloVerde/CicloVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.futurodev.cicloVerde.cicloVerde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicloVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
